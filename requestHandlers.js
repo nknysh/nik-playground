cc1ac0262f1dfba8fb89500b3749db550cf9c819
Nikita Knysh <nik@e-conomic.com>
2015-04-10 10:57:32 +0300

    Handle file uploads.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,7 @@
 var querystring = require("querystring");
 var string = require("string");
+var fs = require("fs");
+var formidable = require("formidable");
 
 function start(response, postData) {
 	console.log("Request handler Start was called.");
@@ -14,6 +16,12 @@ var body = '<html>'+
 '<textarea name="text" rows="20" cols="60"></textarea>'+
 '<input type="submit" value="Submit text" />'+
 '</form>'+
+'<br /><br />'+
+'<form action="/upload" enctype="multipart/form-data" '+
+'method="post">'+
+'<input type="file" name="upload">'+
+'<input type="submit" value="Upload file" />'+
+'</form>'+
 '</body>'+
 '</html>';
 
@@ -22,14 +30,41 @@ var body = '<html>'+
 	response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
 	console.log("Request handler Upload was called.");
+	
+	var form = new formidable.IncomingForm();
+	console.log("about to parse");
+	form.parse(request, function(error, fields, files) {
+		console.log("parsing done");
+		
+		fs.rename(files.upload.path, "C:/tmp/test.png", function (error) {
+			if (error) {
+				// fs.unlink("C:/tmp/test.png");
+				fs.rename(files.upload.path, "C:/tmp/test.png");
+			}
+		});
+
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write("received image:<br/>");
+	response.write("<img src='/show' />");
+	response.end();
+
+	/*
 	response.writeHead(200, {"Content-Type": "text/html"});
 	var text = querystring.parse(postData).text;
 
 	response.write("WOW! You've sent the text: " + text + ", size = " + string(text).length + "<br />");
 	response.write("isValid = " + string(isValid(text)).toString());
 	response.end();
+	*/
+	});
+}
+
+function show(response) {
+	console.log("Request handler Show was called.");
+	response.writeHead(200, {"Content-Type": "image/png"});
+	fs.createReadStream("C:/tmp/test.png").pipe(response);
 }
 
 function isValid(myString) {
@@ -43,4 +78,5 @@ function isValid(myString) {
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
 exports.isValid = isValid;
--- server.js
+++ server.js
@@ -4,20 +4,22 @@ var url = require("url");
 
 function start(route, handle) {
 	function onRequest(request, response) {
-		var postData = "";
+		// var postData = "";
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
 		
-		request.setEncoding("utf8");
+		// request.setEncoding("utf8");
 
+		/*
 		request.addListener("data", function(postDataChunk) {
 			postData += postDataChunk;
 			console.log ("\n\nReceived POST data chunk '" + postDataChunk + "',");
 		});
+*/
 
-		request.addListener("end", function() {
-			route(handle, pathname, response, postData);
-		});
+		// request.addListener("end", function() {
+			route(handle, pathname, response, request);
+		// });
 	}
 
 
